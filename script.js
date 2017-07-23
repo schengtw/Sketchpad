ae132a812898f0391d8aa1e4e4b874869999a761
Sarah Cheng <saiyuki8820@yahoo.com.tw>
2017-07-23 00:19:17 +0000

    Make squares with JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    for (var i=0; i<16; i++){
+        $('table').append('<tr></tr>');
+    }
+
+    for (var i=0; i<16; i++){
+     $('tr').append('<td></td>');
+    }
+
   $('td').mouseenter(function(){
     $(this).addClass('change-background-color');
   });
