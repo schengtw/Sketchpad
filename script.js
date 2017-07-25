eb4eff2f84124bb4e13a75c139068984ad28cbb9
Sarah Cheng <saiyuki8820@yahoo.com.tw>
2017-07-25 00:38:54 +0000

    Enable buttons for changing colors

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-$(document).ready(function(){
-    for (var i=0; i<16; i++){
+$(document).ready(function () {
+    for (var i = 0; i < 16; i++) {
         $('table').append('<tr></tr>');
     }
 
-    for (var i=0; i<16; i++){
-     $('tr').append('<td></td>');
+    for (var i = 0; i < 16; i++) {
+        $('tr').append('<td></td>');
     }
 
-  $('td').mouseenter(function(){
-    $(this).addClass('change-background-color');
-  });
+    var className = 'single';
+
+    $('.clear').click(function () {
+        $('td').removeClass();
+    });
+
+    $('.one-color').click(function () {
+        className = 'single';
+    });
+
+    $('.two-color').click(function () {
+        className = 'alternating-0';
+    });
+
+    $('.many-colors').click(function () {
+    });
+
+    $('td').mouseenter(function () {
+        $(this).removeClass();
+        $(this).addClass(className);
+        if (className === 'alternating-0') {
+           className = 'alternating-1';
+        } else if (className === 'alternating-1') {
+            className = 'alternating-0';
+        }
+    });
 });
